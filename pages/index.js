74c69b96181344bae6c3051f60ffd8c9f8b7e84f
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-07-15 08:56:07 -0300

    dia 2 e 3

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,70 @@
+import { useState, useEffect } from "react";
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
 import {
   AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
   OrkutNostalgicIconSet,
 } from "../src/lib/AluracutCommons";
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar({ githubUser }) {
   return (
-    <Box>
+    <Box as="aside">
       <img
         src={`https://github.com/${githubUser}.png`}
         alt=""
         style={{ borderRadius: "8px" }}
       />
+      <hr />
+      <a className="boxLink" href={`https://github.com/${githubUser}.png`}>
+        @{githubUser}
+      </a>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
 
+function ProfileRelationsBox({ title, items }) {
+  return (
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {title} ({items.length})
+      </h2>
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item.id}>
+              <a href={`/users/${title}`}>
+                <img src={item.image} alt={item.image} />
+                <span>{item.title}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </ProfileRelationsBoxWrapper>
+  );
+}
+
 export default function Home() {
+  const [comunidades, setComunidades] = useState([
+    {
+      id: "123456456879456",
+      title: "Eu odeio acordar cedo",
+      image: "https://alurakut.vercel.app/capa-comunidade-01.jpg",
+    },
+  ]);
+  const [seguidores, setSeguidores] = useState([]);
   const githubUser = "bryanbruzinga";
   const pessoasFavoritas = ["bryanbruzinga", "bryanbruzinga", "bryanbruzinga"];
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/bryanbruzinga/followers")
+      .then((r) => r.json())
+      .then((json) => setSeguidores(json));
+  }, []);
 
   return (
     <>
@@ -35,29 +79,59 @@ export default function Home() {
 
             <OrkutNostalgicIconSet />
           </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form
+              onSubmit={function handleCriaComunidade(event) {
+                event.preventDefault();
+                const dadosDoForm = new FormData(event.target);
+
+                console.log(dadosDoForm.get("title"));
+
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get("title"),
+                  image: dadosDoForm.get("image"),
+                };
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas);
+              }}
+            >
+              <div>
+                <input
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+
+              <div>
+                <input
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa?"
+                  placeholder="Coloque uma URL para usarmos de capa?"
+                  type="text"
+                />
+              </div>
+
+              <button>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
         <div
           className="profileRelationsArea"
           style={{ gridArea: "profileRelationsArea" }}
         >
-          <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-              Pessoas da comunidade ({pessoasFavoritas.length})
-            </h2>
-            <ul>
-              {pessoasFavoritas.map((item, index) => {
-                return (
-                  <li>
-                    <a href={`/users/${githubUser}`} key={index}>
-                      <img src={`https://github.com/${githubUser}.png`} />
-                      <span>{githubUser}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </ProfileRelationsBoxWrapper>
-          <Box>Comunidades</Box>
+          <ProfileRelationsBox title="Seguidores" items={seguidores} />
+
+          <ProfileRelationsBox
+            title="Pessoas da comunidade"
+            items={pessoasFavoritas}
+          />
+
+          <ProfileRelationsBox title="Comunidades" items={comunidades} />
         </div>
       </MainGrid>
     </>
